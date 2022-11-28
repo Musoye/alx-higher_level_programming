07f69c48cc00baef36135a0575d0a72527972dfa
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-11-28 08:50:25 -0800

    linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * check_cycle - check if linked list is cyclic
+ * @list: the linked list
+ * Return: 0 on False and 1 if true
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+	int *address = &list;
+
+	while (current != address)
+	{
+		if (current->next == NULL)
+		{
+			return (0);
+		}
+		current = current->next;
+	}
+	return (1);
+}
