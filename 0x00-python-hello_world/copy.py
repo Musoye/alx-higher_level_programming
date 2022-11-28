bcc7152cf8cc3f510c2c5ef98bd3dd2c7703e57d
Musoye <oyebamijimustapha44@gmail.com>
2022-11-28 12:43:10 +0000

    Copy - Cut - Paste

---
+++ 0x00-python-hello_world/copy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_2 = word[7:]
+middle_word = word[4]
+print(word_first_3)
+print(word_last_2)
+#print(f"Middle word: {middle_word}")
