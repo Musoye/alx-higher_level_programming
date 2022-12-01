92347b788b71314cedc89c530830ba62a84c3297
Musoye <oyebamijimustapha44@gmail.com>
2022-12-01 12:54:26 +0000

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,7 +6,5 @@ if __name__ == "__main__":
 
     var = dir(hidden_4)
     for i in var:
-        if i[0] == '_' and i[1] == '_':
-            continue
-        else:
+        if i[:2] != "__":
             print(i)
