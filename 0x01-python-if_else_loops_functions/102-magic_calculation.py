117a12cd74a7195bb476c4c1cac68f5a3c0a24d0
Musoye <oyebamijimustapha44@gmail.com>
2022-12-01 21:29:21 +0000

    ByteCode -> Python #2

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import dis
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    else:
+        if c > b:
+            return (a + b)
+        else:
+            return (a * b - c)
+dis.dis(magic_calculation)
