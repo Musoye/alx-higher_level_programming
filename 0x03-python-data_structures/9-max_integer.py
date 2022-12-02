f739e91c418795b9b6d49e7256b9e92ec95e02ba
Musoye <oyebamijimustapha44@gmail.com>
2022-12-02 14:22:10 +0000

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """find the max element in list
+
+    Args:
+        my_list: the list that contains the int
+
+    Returns:
+        return the max element
+    """
+    min_value = -10000000
+    for i in my_list:
+        if i > min_value:
+            min_value = i
+    return (min_value)
