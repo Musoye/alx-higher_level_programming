cc2ef644b0c29c228978933ca28fd49067aa460d
Musoye <oyebamijimustapha44@gmail.com>
2022-12-02 14:07:51 +0000

    More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """calculate length of stence and first character
+
+    Args:
+        sentence: the sentence
+
+    Returns:
+        return a (length, first-char)
+    """
+    if len(sentence) == 0:
+        result = 0, None
+    result = len(sentence), sentence[0]
+    return (result)
