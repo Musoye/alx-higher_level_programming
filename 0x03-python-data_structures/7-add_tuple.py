dec26105ff22b1ff9d17431373729d5ce2c1377c
Musoye <oyebamijimustapha44@gmail.com>
2022-12-06 16:16:10 +0000

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,23 +10,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
         the resulr of sum of two first  elment of
         each tuple
     """
-    c = 0
-    d = 0
-    if len(tuple_a) == 0:
-        a = 0
-        b = 0
-    elif len(tuple_a) == 1:
-        b = 0
-    else:
-        a = tuple_a[0] if tuple_a[0] != '' else 0
-        b = tuple_a[1] if tuple_a[1] != '' else 0
-    if len(tuple_b) == 0:
-        c = 0
-        d = 0
-    elif len(tuple_b) == 1:
-        d = 0
-    else:
-        c = tuple_b[0] if tuple_b[0] != '' else 0
-        d = tuple_b[1] if tuple_b[1] != '' else 0
-    result = a + c, b + d
-    return (result)
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
