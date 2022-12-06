81b1985dd871825e758db158d8bbc11048ab68b3
Musoye <oyebamijimustapha44@gmail.com>
2022-12-06 16:30:15 +0000

    Print a list of integers... in reverse!

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,7 +8,7 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
         Null
     """
-    if my_list == None:
+    if my_list is None:
         return ('')
     if len(my_list) == 0:
         return ('')
