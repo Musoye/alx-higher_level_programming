5562a0a120e98563247a8a92664f29abc04d5899
Musoye <oyebamijimustapha44@gmail.com>
2022-12-06 11:23:03 +0000

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -10,5 +10,6 @@ def multiple_returns(sentence):
     """
     if len(sentence) == 0:
         result = 0, None
-    result = len(sentence), sentence[0]
+    else:
+        result = len(sentence), sentence[0]
     return (result)
