7940316bb324ea44a91e1ca98e14cd0561ca0e7c
Musoye <oyebamijimustapha44@gmail.com>
2022-12-07 12:23:30 +0000

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """search a list and replace it with replace
+
+    Args:
+        my_list: the list to search through
+        search: the element to search for
+        replace: the elemnt to replace it
+
+    Returns:
+        the new list with replace element
+    """
+    new_list = []
+    if len(my_list) == 0 or my_list is None:
+        return (None)
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return (new_list)
