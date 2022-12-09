0c3fae9990877ccb2065e65ba8332178abf71668
Musoye <oyebamijimustapha44@gmail.com>
2022-12-09 14:56:48 +0000

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """Roman to Int
+
+    Args:
+        roman_string: the roman string
+
+    Returns:
+        the arabic numeral
+    """
+    roman = ['M', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX',
+            'V', 'IV', 'I']
+    arabic = [1000, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    length = len(roman_string)
+    number = 0
+    a = 0
+    while length != 0:
+        if length == 1:
+            idx = roman.index(roman_string[a])
+            number += arabic[idx]
+            return (number)
+        try:
+            idx = roman.index(roman_string[a: a + 2])
+            number += arabic[idx]
+            length -= 2
+            a += 2
+        except ValueError:
+            idx = roman.index(roman_string[a])
+            number += arabic[idx]
+            length -= 1
+            a += 1
+    return (number)
