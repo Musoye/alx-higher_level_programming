71607c45779b58ac477f36a619d068be096dfb3e
Musoye <oyebamijimustapha44@gmail.com>
2022-12-19 20:06:25 +0000

    ByteCode -> Python #4'

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -14,6 +14,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except:
+    except sys.exc_info()[0]:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
