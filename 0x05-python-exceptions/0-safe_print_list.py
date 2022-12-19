f04de593399868acbe146c4f10401b49cb17269d
Musoye <oyebamijimustapha44@gmail.com>
2022-12-19 11:18:54 +0000

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -18,5 +18,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             i += 1
     except IndexError:
-    finally:
+        print("")
         return (i)
+    print("")
+    return (i)
