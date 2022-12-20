12fbe0d4b4f109483285b2f564bf37a1b964a934
Musoye <oyebamijimustapha44@gmail.com>
2022-12-20 22:06:18 +0000

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,76 +1,60 @@
 #!/usr/bin/python3
-"""The square module continue"""
+
+"""Define a class Square."""
 
 
 class Square:
-    """The class to create square."""
+    """Represent a square."""
 
-    def __init__(self, size=0, position=(0,0)):
-        """set size to private instance variable
-
-        Args:
-            size (int): the size of the square
-        """
-        self.__size = size
-        self.__position = position
-
-    def area(self):
-        """get the area of the square
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
 
         Args:
-            None
-
-        Returns:
-            Area of the square(int)
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
-        return (self.__size * self.__size)
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """getter that get size variable"""
+        """Get/set the current size of the square."""
         return (self.__size)
-
-    @property
-    def position(self):
-        """getter that get the position variable"""
-        return (self.__position)
-
-    @position.setter
-    def position(self, value):
-        """resert the position
-
-        Args:
-            value: the position
-        """
-        if (not isinstance(value, tuple)
-            or len(value) != 2
-            or not all(isinstance(num, int) for num in value)
-            or not all(num > 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     @size.setter
     def size(self, value):
-        """Set thesize to the value
-
-        Args:
-            value: the value to reset
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
     def my_print(self):
-        """print square form the size"""
-        value = self.__size
-        if value == 0:
+        """Print the square with the # character."""
+        if self.__size == 0:
             print("")
             return
 
-        [print("") for m in range(self.__position[1])]
-        for i in range(value):
-            [print(' ', end='') for k in range(self.__position[0])]
-            [print('#', end='') for j in range(value)]
-            print('')
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
