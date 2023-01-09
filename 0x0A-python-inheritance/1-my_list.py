2f87cfd066e27fdb2abed10a8fe3f41afe39552e
musoye <oyebamijimustapha44@gmail.com>
2023-01-09 11:53:20 +0000

    MyList'

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,12 +3,12 @@
 
 
 class MyList(list):
-      """created a sorted list"""
+    """created a sorted list"""
 
-      def __init__(self):
-          """constructor method"""
-          super().__init__()
+    def __init__(self):
+        """constructor method"""
+        super().__init__()
 
-      def print_sorted(self):
-          """print a sorted list"""
-          print(sorted(self))
+    def print_sorted(self):
+        """print a sorted list"""
+        print(sorted(self))
