45f5b0f6885e95f1f9680ecb100693707baac379
musoye <oyebamijimustapha44@gmail.com>
2023-01-10 11:45:10 +0000

    Create object from a JSON file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,4 +11,4 @@ def save_to_json_file(my_obj, filename):
         filename: the file to write to
     """
     with open(filename, 'w') as myfile:
-        json.dump(my_obj, filename)
+        json.dump(my_obj, myfile)
