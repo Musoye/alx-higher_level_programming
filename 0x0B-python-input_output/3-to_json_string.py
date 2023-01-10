87dc48909bc0771cedb2fe386c66e04c6c5de716
musoye <oyebamijimustapha44@gmail.com>
2023-01-10 11:07:19 +0000

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""return JSON representation of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """to json strinf
+
+    Args:
+        my_obj: the python data structure
+
+    Returns:
+        thr json object representation
+    """
+    return (json.dump(my_obj))
