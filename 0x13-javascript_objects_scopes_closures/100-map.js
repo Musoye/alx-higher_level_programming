589f209667c90f9ddabd9eafe70c5ec87f3ef6f3
musoye <oyebamijimustapha44@gmail.com>
2023-03-14 04:45:27 -0700

    Factor index update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let list = require('./100-data.js').list;
-let arr = list.map((element, index) => element * index);
+const list = require('./100-data.js').list;
+const arr = list.map((element, index) => element * index);
 console.log(list);
 console.log(arr);
