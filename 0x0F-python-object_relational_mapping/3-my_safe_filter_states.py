f415215c56e089f5b89c25236cf4180ba7e2fd74
musoye <oyebamijimustapha44@gmail.com>
2023-03-17 00:46:36 -0700

    General: pycodestyle validation

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     value = sys.argv[4]
-    cur.execute("SELECT * FROM `states` WHERE name LIKE %s ORDER BY `id` ASC", (value,))
+    cur.execute("SELECT * FROM `states` WHERE name\
+            LIKE %s ORDER BY `id` ASC", (value,))
     for i in cur.fetchall():
         print(i)
     cur.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,10 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` FROM `cities` INNER JOIN `states` ON `cities`.`state_id` = `states`.`id` ORDER BY `cities`.`id` ASC")
+    cur.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`\
+            FROM `cities` INNER JOIN `states` ON\
+            `cities`.`state_id` = `states`.`id`\
+            ORDER BY `cities`.`id` ASC")
     for i in cur.fetchall():
         print(i)
     cur.close()
