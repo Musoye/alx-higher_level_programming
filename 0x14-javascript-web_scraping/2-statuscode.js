2b29c5672edf61d512e0cf6e3951b9ace2a9d6d1
Musoye <oyebamijimustapha44@gmail.com>
2023-04-25 15:55:18 -0700

    status updated

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 const file = process.argv;
 request(file[2], function (error, response) {
   if (error) console.log(error);
-  console.log('code: ', response && response.statusCode);
+  else { console.log('code: ', response.statusCode); }
 });
